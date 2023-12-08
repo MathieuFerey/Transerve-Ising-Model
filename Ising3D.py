9536b0b4b59a143c4a85d5e39ede81c67704c967
Mathieu <ferey.mathieu@gmail.com>
2023-12-08 21:42:44 +0100

    failed phase transition

--- Ising3D.py
+++ Ising3D.py
@@ -20,11 +20,10 @@ def random_lattice3D(Nx, Ny, Nz) :
     return lattice
 
 
-def energy3D(config, J, Gamma, beta) :
+def energy3D(config, J, Gamma, beta_cl) :
 
     Nx,Ny,Nz = np.shape(config)
-    K = beta*J
-    Km = 0.5*np.log(1/np.tanh(beta*Gamma/Nz))
+    Km = 0.5/beta_cl*np.log(1/np.tanh(beta_cl*Gamma))
 
     H1 = 0
     H2 = 0
@@ -35,21 +34,24 @@ def energy3D(config, J, Gamma, beta) :
                 H1 += config[i,j,k]*(config[(i+1)%Nx,j,k] + config[i,(j+1)%Ny,k])
                 H2 += config[i,j,k]*config[i,j,(k+1)%Nz]
 
-    return -K/Nz*H1 - Km*H2
+    return -J*H1 - Km*H2
 
 
-def delta_energy_flip3D(config, a, b, c, J, Gamma, beta) :
+def mag3D(config) :
+    return np.sum(config)
+
+
+def delta_energy_flip3D(config, a, b, c, J, Gamma, beta_cl) :
 
     Nx,Ny,Nz = np.shape(config)
-    K = beta*J
-    Km = 0.5*np.log(1/np.tanh(beta*Gamma/Nz))
+    Km = 0.5/beta_cl*np.log(1/np.tanh(beta_cl*Gamma))
 
-    dE = 2*config[a,b,c]*(K/Nz*(config[(a+1)%Nx,b,c] + config[a,(b+1)%Ny,c] + config[(a-1)%Nx,b,c] + config[a,(b-1)%Ny,c]) + Km*(config[a,b,(c+1)%Nz] + config[a,b,(c-1)%Nz]))
+    dE = 2*config[a,b,c]*(J*(config[(a+1)%Nx,b,c] + config[a,(b+1)%Ny,c] + config[(a-1)%Nx,b,c] + config[a,(b-1)%Ny,c]) + Km*(config[a,b,(c+1)%Nz] + config[a,b,(c-1)%Nz]))
     
     return dE
 
 
-def MC_step3D(config, J, Gamma, beta) : # flips one spins and accept the new config with a certain probability
+def MC_step3D(config, J, Gamma, beta_cl) : # flips one spins and accept the new config with a certain probability
 
     Nx,Ny,Nz = np.shape(config)
 
@@ -59,7 +61,7 @@ def MC_step3D(config, J, Gamma, beta) : # flips one spins and accept the new con
     b = np.random.randint(low=0,high=Ny)
     c = np.random.randint(low=0,high=Nz)
 
-    dE = delta_energy_flip3D(config,a,b,c,J,Gamma,beta)
+    dE = delta_energy_flip3D(config,a,b,c,J,Gamma,beta_cl)
 
     if dE<0 :
         config[a,b,c] *= -1
@@ -67,7 +69,7 @@ def MC_step3D(config, J, Gamma, beta) : # flips one spins and accept the new con
 
     else :
 
-        p = np.exp(-beta*dE)
+        p = np.exp(-beta_cl*dE)
 
         if np.random.uniform() < p: # accept candidate if alpha>=1 or accept candidate with proba alpha if 0<alpha<1
 
@@ -78,3 +80,56 @@ def MC_step3D(config, J, Gamma, beta) : # flips one spins and accept the new con
         else: 
             
             return config,dE,False
+
+
+def MC_configurations(Nx, Ny, Nz, J, Gamma, beta_cl, Neq, Nsteps, plot_MC = False) : # generate Nsteps spin configurations with the MC algorithm with Neq equilibration steps 
+
+    M_burnin = np.zeros(Neq+Nsteps)        
+    E_burnin = np.zeros(Neq+Nsteps+1)
+
+    config = random_lattice3D(Nx,Ny,Nz)
+    E_burnin[0] = energy3D(config,J,Gamma,beta_cl)
+
+    # reach equilibrium
+    for j in range(Neq) :
+        config,dE,accepted = MC_step3D(config,J,Gamma,beta_cl)
+        M_burnin[j] = np.abs(mag3D(config))
+        if accepted :
+            E_burnin[1+j] = E_burnin[j] + dE
+        else :
+            E_burnin[1+j] = E_burnin[j]
+
+    # sample for average
+    for k in range(Nsteps) :
+        config,dE,accepted = MC_step3D(config,J,Gamma,beta_cl)
+        M_burnin[Neq+k] = np.abs(mag3D(config))
+        if accepted :
+            E_burnin[Neq+1+k] = E_burnin[Neq+1+k-1] + dE
+        else :
+            E_burnin[Neq+1+k] = E_burnin[Neq+1+k-1]
+
+    if plot_MC :
+        fig, ax = plt.subplots(1,2,figsize=(15,3))
+
+        ax[0].set_xlabel('MC step')
+        ax[1].set_xlabel('MC step')
+        ax[0].set_ylabel(r'$M/N$')
+        ax[1].set_ylabel(r'$E/N$')
+
+        ax[0].set_xscale('log')
+        ax[1].set_xscale('log')
+
+        ax[0].set_ylim([np.min(M_burnin/(Nx*Ny*Nz))-0.1,np.max(M_burnin/(Nx*Ny*Nz))+0.1])
+        ax[1].set_ylim([np.min(E_burnin/(Nx*Ny*Nz))-0.1,np.max(E_burnin/(Nx*Ny*Nz))+0.1])
+
+        ax[0].plot(range(1,Neq+Nsteps),M_burnin[1:]/(Nx*Ny*Nz))
+        ax[1].plot(range(1,Neq+Nsteps+1),E_burnin[1:]/(Nx*Ny*Nz))
+
+        ax[0].plot([Neq,Neq],[np.min(M_burnin/(Nx*Ny*Nz))-0.1,np.max(M_burnin/(Nx*Ny*Nz))+0.1],'--',color='black')
+        ax[1].plot([Neq,Neq],[np.min(E_burnin/(Nx*Ny*Nz))-0.1,np.max(E_burnin/(Nx*Ny*Nz))+0.1],'--',color='black')
+
+    Mt = M_burnin[Neq:]
+    Et = E_burnin[Neq:]
+
+    return Mt,Et
+
